a74efa147955a51393fcde1c0062ff76e2a049b1
Walhfort <mael.seguenot@gmail.com>
2019-08-11 14:03:26 +0200

    linter

--- helpers.js
+++ helpers.js
@@ -1,10 +1,10 @@
 module.exports = {
-    extractCommand: function (bot, message) {
-        let content = message.content.replace('  ', ' ');
-        let messageArray = content.split(' ');
-        return {
-            cmd: messageArray[1],
-            args: messageArray.slice(2)
-        };
+  extractCommand: function (bot, message) {
+    const content = message.content.replace('  ', ' ')
+    const messageArray = content.split(' ')
+    return {
+      cmd: messageArray[1],
+      args: messageArray.slice(2)
     }
+  }
 }
